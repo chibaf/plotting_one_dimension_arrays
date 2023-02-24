5876cd05bfc712787375c488608f8777bdc80684
chibaf <chibaf@mac.com>
2023-02-24 23:00:22 +0900

    Add files via upload

--- fileplot.py
+++ fileplot.py
@@ -15,6 +15,6 @@ for i in range(1,nd+1):
 fig,ax = plt.subplots(facecolor="w")
 x=range(0,len(listd[0]))
 for i in range(0,nd):
-  plt.plot(x,listd[i],label=str(sys.argv[i+1]))
+  plt.plot(x,listd[i],label=str(i+1)+" "+str(sys.argv[i+1]))
 ax.legend()
 plt.show()
